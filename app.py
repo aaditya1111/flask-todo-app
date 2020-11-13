8060bf007c9a74156ce0c83a558f2d976e796b70
Aaditya Kumar Rai <raiaaditya999@gmail.com>
2020-11-13 15:09:48 +0530

    commit

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+from flask.helpers import url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    # show all todos
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    # add new item
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
